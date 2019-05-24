39f858e28444ea126685a5e59e59ce84f7aefe6a
Pramesh <pramesh@techsevin.com>
2019-05-24 11:32:14 +0530

    First time Image Slider Project added

---
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.imageslider;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,R.drawable.img5,R.drawable.img1,R.drawable.img2};
+    MyCustomPagerAdapter myCustomPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        myCustomPagerAdapter = new MyCustomPagerAdapter(MainActivity.this, images);
+        viewPager.setAdapter(myCustomPagerAdapter);
+
+        Button closebtn = findViewById(R.id.closebtn);
+
+
+        closebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+
+}
